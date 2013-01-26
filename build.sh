2695f4b781677a1dd7f27a1e6ad905905b30ba6c
dinkar ganti <dinkar.ganti@gmail.com>
2013-01-25 23:02:58 -0500

    Use ocamlbuild..much better

--- build.sh
+++ build.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
+ocamlbuild -libs Unix,Str SQLToHaxe.byte
 
-ocamlyacc SQLToHaxeParser.mly
-ocamllex SQLToHaxeLexer.mll
-ocamlc -c SQLToHaxeParser.mli
 
-ocamlc -c GeneratorConstants.ml
-ocamlc -c ColumnDefinition.ml
-ocamlc -c TableDefinition.ml
-ocamlc -c SQLToHaxeParser.ml
-ocamlc -c SQLToHaxeLexer.ml
-ocamlc -c SQLToAsGenerator.ml
-ocamlc -c SQLtoHaxeGenerator.ml
-ocamlc -c SQLToJavaDBGenerator.ml
-ocamlc -c SQLToJavaDTOGenerator.ml
-ocamlc -c FlexGenerator.ml
-ocamlc -g -o SQLToHaxe unix.cma str.cma GeneratorConstants.cmo ColumnDefinition.cmo TableDefinition.cmo SQLToHaxeParser.cmo SQLToHaxeLexer.cmo SQLToAsGenerator.cmo SQLToHaxeGenerator.cmo SQLToJavaDBGenerator.cmo SQLToJavaDTOGenerator.cmo FlexGenerator.cmo SQLToHaxe.ml 
